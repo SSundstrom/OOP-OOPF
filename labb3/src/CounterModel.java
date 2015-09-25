62e57bc4d716a7850efc43d54d99261e14e17d4c
fFelixja <felixja@student.chalmers.se>
2015-09-25 10:38:40 +0200

    equals, modulus, decrement done

--- labb3/src/CounterModel.java
+++ labb3/src/CounterModel.java
@@ -1,27 +1,63 @@
+import java.util.IllegalFormatException;
+import java.util.Objects;
+
 public class CounterModel {
+    private int value;
+    private int modulus;
+    private static int NbrOfCounters;
 
-    private int value = 0;
-    private static int nbrOfCounters;
-
-    public CounterModel() {
-        nbrOfCounters++;
+    public CounterModel(){
+        this(10);
     }
-
-    public void increment () {
-        value++;
-    }
-    public void decrement() {
-        value--;
-    }
-    public void reset() {
-        value = 0;
-    }
-    public int getValue() {
-        return value;
-    }
-    public static int getNbrOfCounters() {
-        return nbrOfCounters;
+    public CounterModel(int max){
+        if(max < 0){
+            throw new IllegalArgumentException();
+        }
+        this.modulus = max;
+        this.value = 0;
+        this.NbrOfCounters++;
     }
 
+    public void increment(){
+        this.value = ((value + 1)%modulus);
+    }
+    public void decrement(){
+        if(this.value == 0){
+            this.value = this.modulus - 1;
+        }else {
+            this.value = this.value - 1;
+        }
 
+    }
+    public void reset(){
+        this.value = 0;
+
+    }
+    public String toString(){ //TODO
+
+
+        return "";
+    }
+    public boolean equals(Object cm){
+        if(this == cm){
+            return  true;
+        }else if (cm == null || this.getClass() != cm.getClass()){
+            return false;
+        }else{
+            CounterModel n = (CounterModel)cm;
+            return (n.getValue() == this.getValue()) && (n.getModulus() == this.getModulus());
+        }
+    }
+
+    public int getModulus(){
+        return this.modulus;
+    }
+    public int getValue(){
+        return this.value;
+
+    }
+    public static int getNbrOfCounters(){
+        return NbrOfCounters;
+
+    }
 }
--- labb3/src/TestCounter.java
+++ labb3/src/TestCounter.java
@@ -9,7 +9,99 @@ public class TestCounter {
 
         CounterModel counterA = new CounterModel();
         counters.add(counterA);
+        CounterModel counterB = new Coimport java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+
+public class TestCounter {
+
+    public static void main(String[] args) {
+
+        List<CounterModel> counters = new LinkedList<>();
+
+        CounterModel counterA = new CounterModel();
+        counters.add(counterA);
         CounterModel counterB = new CounterModel();
+        counters.add(counterB);
+        CounterModel counterC = new CounterModel();
+        counters.add(counterC);
+        CounterModel counterD = new CounterModel();
+        counters.add(counterD);
+        CounterModel counterE = new CounterModel();
+        counters.add(counterE);
+        CounterModel counterF = new CounterModel();
+        counters.add(counterF);
+        Object counterG = new CounterModel();
+        counters.add((CounterModel)counterG);
+        CounterModel counterH = new CounterModel();
+        counters.add(counterH);
+
+
+
+        //     Changes in counterA
+        for (int i = 0; i < 6; i++) {
+            counterA.increment();
+        }
+        //     Changes in counterB
+        for (int i = 0; i < 10; i++) {
+            counterB.decrement();
+        }
+        //     Changes in counterC
+        for (int i = 0; i < 5; i++) {
+            counterC.decrement();
+        }
+        counterC.reset();
+        //     Changes in counterD
+        for (int i = 0; i < 19; i++) {
+            counterD.increment();
+        }
+        //     Changes in counterE
+        while (counterE.getValue() < counterE.getModulus() - 5) {
+            counterE.decrement();
+            counterE.increment();
+            counterE.increment();
+        }
+        //     Changes in counterF
+        counterF.increment();
+        while (counterF.getValue() > 0)
+            counterF.reset();
+
+
+        if (test("A", counterA, 6) &&
+                test("B", counterB, 0) &&
+                test("C", counterC, 0) &&
+                test("D", counterD, 9) &&
+                test("E", counterE, 5) &&
+                test("F", counterF, 0) &&
+                testNbrOfCounters(counters)){
+
+            System.out.println("All tests cleared.");
+        }
+       if(counterH.equals(counterF)){
+           System.out.println("Equals passed");
+        }else{
+           System.out.println("Equals failed");
+       }
+    }
+    public static boolean test(String nr, CounterModel counter, int ans) {
+        if (ans == counter.getValue()) {
+            System.out.println("counter" + nr + " test cleared");
+            return true;
+        }
+        System.out.println("Wrong answer in counter" + nr);
+        return false;
+    }
+
+    public static boolean testNbrOfCounters(List counters) {
+        if (CounterModel.getNbrOfCounters() == counters.size()) {
+            System.out.println("Number of counters test cleared");
+            return true;
+        }
+        System.out.println("Not picking up the correct amount of counters");
+        return false;
+    }
+}
+unterModel();
         counters.add(counterB);
         CounterModel counterC = new CounterModel();
         counters.add(counterC);
