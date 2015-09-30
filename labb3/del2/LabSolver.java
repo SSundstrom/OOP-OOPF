e9caf584d4e66ce8174d05b9b5528ca5db5b93ac
fFelixja <felixja@student.chalmers.se>
2015-09-30 17:23:05 +0200

    LabSolver is done added comments

--- labb3/del2/LabSolver.java
+++ labb3/del2/LabSolver.java
@@ -21,33 +21,41 @@ public class LabSolver {
     }
 
     public static boolean findPath(int x0, int y0, int x1, int y1, Labyrinth l) {
+        //If this position is mark this is wrong way
         if(l.getMark(x0,y0)){
             return false;
         }
+        //Set this way as correct
         l.setMark(x0, y0, true);
+        //Checks if we reached correct position
         if(x1 == x0 && y1 == y0 ){
             return true;
         }
+        //Checks if possible to move down
         if(l.canMove(Labyrinth.Direction.DOWN, x0, y0)){
             if(findPath(x0, y0 +1, x1, y1, l)){
                 return true;
             }
         }
+       //Checks if possible to move up
         if(l.canMove(Labyrinth.Direction.UP, x0, y0)){
             if(findPath(x0, y0 - 1, x1, y1, l)){
                 return true;
             }
         }
+       //Checks if possible to move left
         if(l.canMove(Labyrinth.Direction.LEFT, x0, y0)){
             if(findPath(x0 - 1, y0, x1, y1, l)){
                 return true;
             }
         }
+        //Checks if possible to move right
         if(l.canMove(Labyrinth.Direction.RIGHT, x0, y0)){
             if(findPath(x0 + 1 , y0, x1, y1, l)){
                 return true;
             }
         }
+        //Sets way to false because right way already found, no possible way or is in a dead end
         l.setMark(x0,y0,false);
         return false;
     }
