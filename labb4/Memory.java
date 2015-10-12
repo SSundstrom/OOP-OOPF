b9a1b9b1e5f2c18e39def32e456c686ea8c23411
SSundstrom <simsund@student.chalmers.se>
2015-10-12 15:07:22 +0200

    Memory 2 new methods

--- labb4/Memory.java
+++ labb4/Memory.java
@@ -1,25 +1,61 @@
 import javax.swing.*;
+import java.awt.*;
 
-public class Memory {
+public class Memory extends JFrame {
+
+    public static void createCardSlots(int pairs, JFrame f) {
+
+      JPanel box;
+
+      for (int i = 0; i < pairs*2; i++) {
+        box = new JPanel();
+        box.setBackground(Color.yellow);
+        box.setLayout(new GridLayout(4,4));
+        f.add(box);
+      }
+    }
+
+    public static int askWidth() {
+      int totalPairs = 0;
+
+      String nbrPairs = JOptionPane.showInputDialog("How many rows do you want?");
+
+      try {
+          totalPairs = Integer.parseInt(nbrPairs.trim());
+      } catch (NumberFormatException e) {
+          JOptionPane.showMessageDialog(null, "Please enter a number");
+      }
+      return totalPairs;
+    }
+
+    public static int askHeigth() {
+      int totalPairs = 0;
+
+      String nbrPairs = JOptionPane.showInputDialog("How many columns do you want?");
+
+      try {
+          totalPairs = Integer.parseInt(nbrPairs.trim());
+      } catch (NumberFormatException e) {
+          JOptionPane.showMessageDialog(null, "Please enter a number");
+      }
+      return totalPairs;
+    }
+
     public static void createWindow() {
-        int totalPairs = 0;
-        String nbrPairs = JOptionPane.showInputDialog("How many pairs do you want?");
-        try {
-            totalPairs = Integer.parseInt(nbrPairs.trim());
-        } catch (NumberFormatException e) {
-            JOptionPane.showMessageDialog(null, "Please enter a number");
-        }
-        int width = (int)Math.sqrt(totalPairs*2);
-        int height = totalPairs/width;
 
-        width *= 75;
-        height *= 75;
+        int width =
+
+        width *= 100;
+        heigth *= 100;
 
         JFrame frame = new JFrame("Memory");
+        frame.setBackground(Color.RED);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(width, height);
+        frame.setSize(width, heigth);
         frame.setLocation(100, 100);
         frame.setVisible(true);
+
+        createCardSlots(totalPairs, frame);
     }
 
     public static void main(String[] args) {
