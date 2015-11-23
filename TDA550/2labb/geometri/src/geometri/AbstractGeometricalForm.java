1c0d848e8fe9b2cd794a0cfdb23382c416e5bf97
Simon <ssundstrom@live.se>
2015-11-23 16:17:23 +0100

    Created abstartct class

---
+++ TDA550/2labb/geometri/src/geometri/AbstractGeometricalForm.java
@@ -0,0 +1,22 @@
+package geometri;
+
+import java.awt.*;
+
+public abstract class AbstractGeometricalForm implements GeometricalForm {
+    public int getX() {
+
+    }
+    public int getY() {
+
+    }
+    public Color getColor() {
+
+    }
+    public void move(int dx, int dy) throws IllegalPositionException {
+
+    }
+    public void place(int x, int y) throws IllegalPositionException {
+
+    }
+
+}
---
+++ TDA550/2labb/geometri/src/geometri/line2.java
@@ -0,0 +1,36 @@
+package geometri;
+
+import java.awt.*;
+
+public class line2 extends AbstractGeometricalForm {
+
+    @Override
+    public void fill(Graphics g) {
+
+    }
+
+    @Override
+    public int getArea() {
+        return 0;
+    }
+
+    @Override
+    public int getHeight() {
+        return 0;
+    }
+
+    @Override
+    public int getPerimeter() {
+        return 0;
+    }
+
+    @Override
+    public int getWidth() {
+        return 0;
+    }
+
+    @Override
+    public int compareTo(GeometricalForm geometricalForm) {
+        return 0;
+    }
+}
