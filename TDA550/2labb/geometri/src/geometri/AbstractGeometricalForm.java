76371fe7be1811ce53446f1d33ee9b42fa6f7271
Simon <ssundstrom@live.se>
2015-11-24 12:03:59 +0100

    progress

--- TDA550/2labb/geometri/src/geometri/AbstractGeometricalForm.java
+++ TDA550/2labb/geometri/src/geometri/AbstractGeometricalForm.java
@@ -2,21 +2,99 @@ package geometri;
 
 import java.awt.*;
 
+/**
+ * Abstract class for geometrical forms.
+ */
+
 public abstract class AbstractGeometricalForm implements GeometricalForm {
+
+    private int x;
+    private int y;
+    private Color color;
+    private int height;
+    private int width;
+
+    public AbstractGeometricalForm( int x, int y, int width, int height, Color color) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
     public int getX() {
-
+        return x;
     }
+    /**
+     * {@inheritDoc}
+     */
     public int getY() {
-
+        return y;
     }
+    /**
+     * {@inheritDoc}
+     */
     public Color getColor() {
-
+        return color;
     }
+
+    /**
+     * {@inheritDoc}
+     */
     public void move(int dx, int dy) throws IllegalPositionException {
 
     }
+
+    /**
+     * {@inheritDoc}
+     */
+    public int getHeight() {
+        return height;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public int getWidth() {
+        return width;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
     public void place(int x, int y) throws IllegalPositionException {
 
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public int hashCode() {
+        return 0;
+    }
+
+    /**
+     * Compares two geometrical forms and determines if they are in order.
+     * @param geometricalForm
+     * @return
+     */
+
+    public int compareTo(GeometricalForm geometricalForm) {
+        return 0;
+    }
+
+    /**
+     * Compares two objects with each other.
+     * @param o Object to compare with.
+     * @return True if the objects are identical except the position.
+     */
+
+    public boolean equals(Object o){
+        return false;
     }
 
 }
--- TDA550/2labb/geometri/src/geometri/Circle.java
+++ TDA550/2labb/geometri/src/geometri/Circle.java
@@ -6,144 +6,48 @@ import java.util.Objects;
 /**
  * Represents a drawable Circle
  */
-public class Circle implements GeometricalForm {
+public class Circle extends AbstractGeometricalForm {
 
-    private int x;
-    private int y;
     private int diameter;
-    private Color color;
 
     /**
      * Initialize a new Circle on defined position, diameter and color.
-     * @param x Coordinate in x.
-     * @param y Coordinate in y.
+     *
+     * @param x        Coordinate in x.
+     * @param y        Coordinate in y.
      * @param diameter The diameter of the circle.
-     * @param c The color of the square.
+     * @param c        The color of the square.
      * @throws IllegalPositionException Exception if coordinates is illegal.
      */
-    public Circle(int x, int y, int diameter, Color c) throws IllegalPositionException{
-        this.x = x;
-        this.y = y;
+    public Circle(int x, int y, int diameter, Color c) throws IllegalPositionException {
+        super(x, y, diameter, diameter, c);
         this.diameter = diameter;
-        this.color = c;
     }
 
     /**
      * Initialize a new square from an existing GeometricalForm.
-     * @param f GeometricalForm to get coordinates from.
+     *
+     * @param f        GeometricalForm to get coordinates from.
      * @param diameter The diameter of the circle.
-     * @param c The color of the circle.
+     * @param c        The color of the circle.
      */
-    public Circle(GeometricalForm f, int diameter, Color c){
-        this.x = f.getX();
-        this.y = f.getY();
+    public Circle(GeometricalForm f, int diameter, Color c) {
+        super(f.getX(), f.getY(), diameter, diameter, c);
         this.diameter = diameter;
-        this.color = c;
     }
 
-    /**
-     * {@inheritDoc}
-     */
     @Override
     public void fill(Graphics g) {
 
     }
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public Color getColor() {
-        return null;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
     @Override
     public int getArea() {
         return 0;
     }
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public int getHeight() {
-        return 0;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
     @Override
     public int getPerimeter() {
-        return 0;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public int getWidth() {
-        return 0;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public int getX() {
-        return 0;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public int getY() {
-        return 0;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void move(int dx, int dy) throws IllegalPositionException {
-
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void place(int x, int y) throws IllegalPositionException {
-
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public int compareTo(GeometricalForm geometricalForm) {
-        return 0;
-    }
-
-    /**
-     * Compares two objects with each other.
-     * @param o Object to compare with.
-     * @return True if the objects are identical except the position.
-     */
-    @Override
-    public boolean equals(Object o){
-        return false;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public int hashCode(){
         return 0;
     }
 }
