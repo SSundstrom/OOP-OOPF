7fcc53717040dc81142a5e17bf07b21d4950bb36
ffelixja <felixja@student.chalmers.se>
2015-11-24 12:39:53 +0100

    Rectangle and square done

--- TDA550/2labb/geometri/src/geometri/Rectangle.java
+++ TDA550/2labb/geometri/src/geometri/Rectangle.java
@@ -4,42 +4,32 @@ import java.awt.*;
 /**
  * Represents a drawable Rectangle
  */
-public class Rectangle implements GeometricalForm {
-
-    private int x;
-    private int y;
-    private int width;
-    private int height;
-    private Color color;
+public class Rectangle extends AbstractGeometricalForm {
 
     /**
      * Initilaze an new Rectangle.
-     * @param x Coordinate in x.
-     * @param y Coordinate in y.
-     * @param width The width of the Rectangle.
+     *
+     * @param x      Coordinate in x.
+     * @param y      Coordinate in y.
+     * @param width  The width of the Rectangle.
      * @param height The height of the Rectangle.
-     * @param c The color of the Rectangle.
+     * @param c      The color of the Rectangle.
      * @throws IllegalPositionException Exception is coordinates is illegal.
      */
-    public Rectangle(int x, int y, int width, int height, Color c) throws IllegalPositionException{
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-        this.color = c;
+    public Rectangle(int x, int y, int width, int height, Color c) throws IllegalPositionException {
+      super(x,y,width,height,c);
     }
 
     /**
      * Initialize a new Rectangle from existing GeometricalFrom.
-     * @param f GeometricalForm to get coordinates from.
-     * @param width The width of the Rectangle.
+     *
+     * @param f      GeometricalForm to get coordinates from.
+     * @param width  The width of the Rectangle.
      * @param height The height of the Rectangle.
-     * @param c The color of the Rectangle.
+     * @param c      The color of the Rectangle.
      */
-    public Rectangle(GeometricalForm f, int width, int height, Color c){
-        this.x = f.getX();
-        this.y = f.getY();
-        this.color = c;
+    public Rectangle(GeometricalForm f, int width, int height, Color c) {
+        super(f.getX(),f.getY(),width,height,c);
     }
 
     /**
@@ -54,97 +44,16 @@ public class Rectangle implements GeometricalForm {
      * {@inheritDoc}
      */
     @Override
-    public Color getColor() {
-        return null;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
     public int getArea() {
         return 0;
     }
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public int getHeight() {
-        return 0;
-    }
 
     /**
      * {@inheritDoc}
      */
     @Override
     public int getPerimeter() {
-        return 0;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public int getWidth() {
-        return 0;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public int getX() {
-        return 0;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public int getY() {
-        return 0;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void move(int dx, int dy) throws IllegalPositionException {
-
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void place(int x, int y) throws IllegalPositionException {
-
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public int compareTo(GeometricalForm geometricalForm) {
-        return 0;
-    }
-
-    /**
-     * Compares two objects with each other.
-     * @param o Object to compare with.
-     * @return True if the objects are identical except the position.
-     */
-    @Override
-    public boolean equals(Object o){
-        return false;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public int hashCode(){
         return 0;
     }
 }
