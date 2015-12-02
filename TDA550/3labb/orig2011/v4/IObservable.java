4c86b619fa73df7621a6f6942809e1be40178b22
ffelixja <felixja@student.chalmers.se>
2015-12-02 16:55:11 +0100

    Done with ReversiModel, goldmodel left in v4

---
+++ TDA550/3labb/orig2011/v4/IObservable.java
@@ -0,0 +1,8 @@
+package orig2011.v4;
+
+import java.beans.PropertyChangeListener;
+
+public interface IObservable {
+    void addObserver(PropertyChangeListener observer);
+    void removeObserver(PropertyChangeListener observer);
+}
